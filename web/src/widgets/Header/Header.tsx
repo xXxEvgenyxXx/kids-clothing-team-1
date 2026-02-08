8949062ff5bd7751a0a754d9a6304f2cca5a80dd
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-08 21:11:40 +0300

    feat: add global.d.ts & header & footer

---
+++ web/src/widgets/Header/Header.tsx
@@ -0,0 +1,15 @@
+import s from './styles/header.scss'
+
+export const Header = () => {
+    return (
+        <header>
+            <h2 className={s.logo}>Эксклюзив</h2>
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </header>
+    )
+}
