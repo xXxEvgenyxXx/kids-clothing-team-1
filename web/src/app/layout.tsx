db9f9f56432381e3586288733f0672b446409c46
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-08 21:52:10 +0300

    feat: add basic structure for header

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -10,7 +10,7 @@ const MainLayout = (props: MainLayoutProps) => {
         <html lang="ru">
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Document</title>
+            <title>Эксклюзив</title>
         </head>
         <body>
             <Header />
--- web/src/widgets/Header/Header.tsx
+++ web/src/widgets/Header/Header.tsx
@@ -1,16 +1,23 @@
 import s from './header.module.scss'
 import Link from 'next/link'
 import { routes } from '@/shared/constants'
+import {Input} from 'antd'
+import {HeartOutlined, ShoppingCartOutlined} from '@ant-design/icons'
 
 export const Header = () => {
     return (
-        <header>
+        <header className={s.header}>
             <h2 className={s.logo}>Эксклюзив</h2>
             <div>
-                
+                <Link href={routes.main}>Главная</Link>
+                <Link href={routes.contacts}>Контакты</Link>
+                <Link href={routes.about}>О нас</Link>
+                <Link href={routes.favorite}>Избранное</Link>
             </div>
             <div>
-
+                <Input placeholder="Что вы ищете?" variant="outlined" />
+                <HeartOutlined />
+                <ShoppingCartOutlined />
             </div>
         </header>
     )
