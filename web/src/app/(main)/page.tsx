e5fea2db6e3227c0edb76699b05c8f1e403cb3d6
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-08 16:22:09 +0300

    feat: add main page

---
+++ web/src/app/(main)/page.tsx
@@ -0,0 +1,18 @@
+import MainLayout from "../layout";
+
+const MainPage = () => {
+    
+    return (
+        <html lang="ru">
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            1234
+        </body>
+        </html>
+    );
+};
+
+export default MainPage;
