42ee3485ab5144429b66a4c53e5e9212ff455940
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-12 12:38:46 +0300

    feat: move _app to src

---
+++ web/src/_app.tsx
@@ -0,0 +1,8 @@
+import MainPage from "./app/(main)/page"
+import './app/global.scss'
+
+export default function App() {
+  return (
+    <MainPage />
+  )
+}
--- web/src/app/_app.tsx
+++
@@ -1,8 +0,0 @@
-import MainPage from "./(main)/page"
-import './global.scss'
-
-export default function App() {
-  return (
-    <MainPage />
-  )
-}
