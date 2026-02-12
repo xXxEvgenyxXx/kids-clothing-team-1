9d231b863bea38baa783c27c9440e55357dcd793
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-12 22:28:38 +0300

    fix: css files in build

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,10 +3,11 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'out',
-  turbopack: {
-    root: path.join(__dirname),
-  },
+  //distDir: 'out',
+  //assetPrefix: "./",
+  //turbopack: {
+  //  root: path.join(__dirname),
+  //},
 };
 
 export default nextConfig;
