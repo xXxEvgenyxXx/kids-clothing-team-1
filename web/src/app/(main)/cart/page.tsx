5318062e07d48cc1886f680437de6baf10b8887e
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-15 11:09:47 +0300

    feat: add basic layout for cart page

--- web/src/app/(main)/cart/page.tsx
+++ web/src/app/(main)/cart/page.tsx
@@ -1,7 +1,24 @@
+import s from './cart.module.scss'
+
 const CartPage = () => {
     
     return (
-        <>Корзина</>
+        <div className={s.cartPageWrapper}>
+            <div className={s.cartItemsWrapper}>
+                <div className={s.cartItemsHeader}>
+                    <h2 className={s.cartItemCategory}>Товар</h2>
+                    <h2 className={s.cartItemCategory}>Цена</h2>
+                    <h2 className={s.cartItemCategory}>Количество</h2>
+                    <h2 className={s.cartItemCategory}>Сумма</h2>
+                </div>
+                <div className={s.cartItems}>
+
+                </div>
+            </div>
+            <div className={s.cartActionsWrapper}>
+
+            </div>
+        </div>
     );
 };
 
