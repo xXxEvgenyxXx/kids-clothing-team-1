39d081e0a8788a24c00bca80514f26dae13d02e2
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-16 11:56:10 +0300

    fix: rollback to /

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
-  assetPrefix: "/kids-clothing-team-1/web/",
+  assetPrefix: "/",
   trailingSlash:true,
   images: {
     unoptimized: true,
