709cf5400e8dd9b7c6db86e6c8ee2bc249f695e9
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-26 23:53:17 +0300

    feat: add layout for contacts page

--- web/src/app/(main)/contacts/page.tsx
+++ web/src/app/(main)/contacts/page.tsx
@@ -1,7 +1,37 @@
+import s from './contacts.module.scss'
+import { PhoneOutlined, MailOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import clsx from 'clsx';
+
 const ContactsPage = () => {
     
     return (
-        <>Контакты</>
+        <div className={s.contactsPageWrapper}>
+            <div className={clsx(s.contactsInfo, s.contactsBlock)}>
+                <div className={s.contactsInfoSection}>
+                    <div className={s.contactsInfoSectionTitle}>
+                        <div className={s.contactsInfoSectionIcon}>
+                            <PhoneOutlined />
+                        </div>
+                        <h2 className={s.contactsInfoSectionHeader}>Звоните нам</h2>
+                    </div>
+                    <p>Мы доступны 24/7. Ждём вашего звонка!</p>
+                    <p>Телефон: +7 965 111 11-11</p>
+                </div>
+                <div className={s.contactsInfoSection}>
+                    <div className={s.contactsInfoSectionTitle}>
+                        <div className={s.contactsInfoSectionIcon}>
+                            <MailOutlined />
+                        </div>
+                        <h2 className={s.contactsInfoSectionHeader}>Напишите нам</h2>
+                    </div>
+                    <p>eksklyuziv@gmail.com</p>
+                </div>
+            </div>
+            <form className={clsx(s.contactsMessage, s.contactsBlock)}>
+
+            </form>
+        </div>
     );
 };
 
