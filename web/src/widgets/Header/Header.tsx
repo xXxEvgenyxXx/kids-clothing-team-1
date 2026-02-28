066d31ec8bb40635f5e02955eb8191f8ebcbf139
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-28 19:23:07 +0300

    feat: add adaptive for main page

--- web/src/widgets/Header/Header.tsx
+++ web/src/widgets/Header/Header.tsx
@@ -1,10 +1,60 @@
+"use client"
+
 import s from './header.module.scss'
 import Link from 'next/link'
 import { routes } from '@/constants'
-import {HeartOutlined, ShoppingCartOutlined} from '@ant-design/icons'
+import {Dropdown, Space} from 'antd'
+import type { MenuProps } from 'antd'
+import {HeartOutlined, ShoppingCartOutlined,  MenuOutlined} from '@ant-design/icons'
 import clsx from 'clsx'
 
+const dropdownItems: MenuProps['items'] = [
+    {
+        label: (
+            <Link href={routes.main}>Главная</Link>
+        ),
+        key: '0'
+    },
+    {
+        label: (
+            <Link href={routes.catalog}>Каталог</Link>
+        ),
+        key: '2'
+    },
+    {
+        label: (
+            <Link href={routes.contacts}>Контакты</Link>
+        ),
+        key: '3'
+    },
+    {
+        label: (
+            <Link href={routes.about}>О нас</Link>
+        ),
+        key: '4'
+    },
+    {
+        label: (
+            <Link href={routes.favorite}>Избранное</Link>
+        ),
+        key: '5'
+    },
+    {
+        label: (
+            <Link href={routes.cart}>Корзина</Link>
+        ),
+        key: '6'
+    },
+]
+
 export const Header = () => {
+    const menuProps: MenuProps = {
+        items: dropdownItems,
+        onClick: (e) => {
+            // Handle click if needed
+        }
+    };
+
     return (
         <header className={s.header}>
             <h2 className={s.logo}>Эксклюзив</h2>
@@ -17,6 +67,15 @@ export const Header = () => {
             <div className={s.headerLinksWrapper}>
                 <Link href={routes.favorite} className={clsx([s.headerLink,s.headerIconLink])}><HeartOutlined /></Link>
                 <Link href={routes.cart} className={clsx([s.headerLink,s.headerIconLink])}><ShoppingCartOutlined /></Link>
+            </div>
+            <div className={s.menuWrapper}>
+                <Dropdown className={s.menu} menu={menuProps} trigger={['click']}>
+                    <a onClick={(e) => e.preventDefault()}>
+                        <Space>
+                            <MenuOutlined/>
+                        </Space>
+                    </a>
+                </Dropdown>
             </div>
         </header>
     )
