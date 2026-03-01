0db29aa1275e0dcbb054fe8aa627c8776afcafa9
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-01 20:33:52 +0300

    feat: copy dependencies to admin

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Remove `output: 'export'` to allow dynamic server routes that read/write
+  // local JSON files during development.
+  distDir: 'out',
+  assetPrefix: "/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
